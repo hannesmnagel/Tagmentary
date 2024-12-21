3ee127ae95dd3620f92dabdb11af283338623f42
Hannes Nagel <hannes.nagel@codeunity.de>
2024-12-21 15:55:26 +0100

    Initial Commit

---
+++ Tagmentary/TagmentaryApp.swift
@@ -0,0 +1,32 @@
+//
+//  TagmentaryApp.swift
+//  Tagmentary
+//
+//  Created by Hannes Nagel on 12/21/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TagmentaryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TagmentaryTests/TagmentaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  TagmentaryTests.swift
+//  TagmentaryTests
+//
+//  Created by Hannes Nagel on 12/21/24.
+//
+
+import Testing
+@testable import Tagmentary
+
+struct TagmentaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
